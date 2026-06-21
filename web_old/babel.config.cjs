4b8b4530e3a10ef25746c793e47317b18dc9f328
Thanga Ganapathy <ganapathy888@gmail.com>
2026-06-21 23:52:23 +0530

    chore: remove web_old (the legacy babel-AST runtime reference)
    The new runtime in packages/web is stable (CSR + SSG, full test suite). The old
    reference copy is no longer needed; nothing imports it.

--- web_old/babel.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: [
-    "@babel/plugin-syntax-jsx",
-    ["./dist/compiler/babel-plugin.cjs", { "runtimeSource": "../index.js" }]
-  ]
-};
--- web_old/runtime/index.js
+++
@@ -1,4 +0,0 @@
-export * from './dom.js';
-export * from './lifecycle.js';
-export * from './props.js';
-export * from './For.js';
